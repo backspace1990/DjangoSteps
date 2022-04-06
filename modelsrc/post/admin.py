347fc8b75cf3f60c6178d25b0a70c5aceda2f97f
ümit <51458506+backspace1990@users.noreply.github.com>
2022-04-06 17:45:46 +0300

    admin panelini ozellestirdik biraz

--- modelsrc/post/admin.py
+++ modelsrc/post/admin.py
@@ -1,3 +1,4 @@
+from re import search
 from django.contrib import admin
 
 # Register your models here.
@@ -12,4 +13,18 @@ from django.contrib import admin
 from post.models import Post
 
 
-admin.site.register(Post)
+# ikinci etki
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publishing_date']
+    #list_display_links = ['title', 'publishing_date'] #  list_editable = ['title'] bu kisim calismasi icin alt satirda title cikarildi
+    #list_display_links = ['publishing_date'] eski hale getirdim alt satirda son yorum satiri ders2 nin
+    list_display_links = ['title', 'publishing_date']
+    list_filter = ['publishing_date']
+    search_fields = ['title', 'content']
+    #list_editable = ['title'] # basiligi deirekt guncellememizi sagliyor 
+
+    class Meta:
+        model = Post
+
+
+admin.site.register(Post, PostAdmin)
