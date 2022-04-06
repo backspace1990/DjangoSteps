2120b4d78940e9ec9598deafb16b3c4730f8c350
ümit <51458506+backspace1990@users.noreply.github.com>
2022-04-06 18:18:44 +0300

    views kismini ayarladik ilk adim views

--- modelsrc/post/views.py
+++ modelsrc/post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 # Icinde python fonksiyonlari yazacagimiz
@@ -7,5 +7,9 @@ from django.shortcuts import render
 # sitesinin bu isteklere cevap vermesi 
 # burada katmanda yazilan fonksiyonlarla
 # saglanir.
+# tr karsiligi goruntu demektir view
 
 # Migrations : Uygulamanin veritabanini tutan yerdir. 
+
+def home_view(request):
+    return HttpResponse('<b><h1>Hosgeldiniz</h1></b>')
