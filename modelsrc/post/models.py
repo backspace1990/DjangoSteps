d7aaa74a4933a9047d72178212fbcf8170fb9681
ümit <51458506+backspace1990@users.noreply.github.com>
2022-04-08 02:42:53 +0300

    Url adresi aktiflestirdik models.py icinde reverse metodunu kullanmayi ogrendim ama tam olarak metodun diger parametrelerine bakmadim

--- modelsrc/post/models.py
+++ modelsrc/post/models.py
@@ -10,18 +10,21 @@ from django.db import models
 #   . Resim vb.
 # bilgilere ihtiyac vardir. Model katmani bu bilgilerin 
 # ve daha fazlasinin tanimlandigi yerdir. 
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=120,verbose_name='Baslik')
     content = models.TextField(verbose_name='Icerik')
     publishing_date = models.DateTimeField(verbose_name='Yayimlanma Tarihi')
 
-
+#return f"/post/detail/{self.id}/"
+#return "/post/index/"
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return f"/post/detail/{self.id}/"
+        return reverse('detail', kwargs={'id': self.id})
     
     def get_index(self):
-        return "/post/index/"
+        return reverse('index')
