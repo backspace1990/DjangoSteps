a8f11a6df2823328f48147d2d863778de0da05e0
ümit <51458506+backspace1990@users.noreply.github.com>
2022-04-13 04:14:03 +0300

    static dosyasi olayini kavradim. Ayrica olusturulan static yapilarini python manage.py collectstatic komutla olusturabiliyoruz

--- modelsrc/src/settings.py
+++ modelsrc/src/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = BASE_DIR / "benim staticlerim"
 #
 #MEDIA_ROOT = BASE_DIR / "uploads"
 #MEDIA_URL = "/images/"
